847b5d84430ac0f3a87a4f0b0acd0eaca0c07d46
Viren Gujariya <viren.gujariya@gmail.com>
2021-04-14 17:57:08 -0700

    more tests

--- app/src/androidTest/java/me/fitbod/repetition/db/PrepopulateWorkoutHistoryImplTest.kt
+++ app/src/androidTest/java/me/fitbod/repetition/db/PrepopulateWorkoutHistoryImplTest.kt
@@ -3,32 +3,34 @@ package me.fitbod.repetition.db
 import android.content.Context
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.platform.app.InstrumentationRegistry
-import org.junit.Before
+import io.kotest.matchers.ints.shouldBeGreaterThan
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldNotBeEmpty
+import me.fitbod.repetition.mappers.LineToWorkoutHistoryEntityMapper
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class PrepopulateWorkoutHistoryImplTest {
     lateinit var prepopulateWorkoutHistory: PrepopulateWorkoutHistory
-
-    @Before
-    fun setup() {
-        val context = ApplicationProvider.getApplicationContext<Context>()
-        // prepopulateWorkoutHistory =
-        //     PrepopulateWorkoutHistoryImpl(InstrumentationRegistry.getInstrumentation().targetContext)
-    }
+    private val mapper = LineToWorkoutHistoryEntityMapper()
 
     @Test
-    fun sdsd() {
+    fun workoutDataIsLoadedIgnoringInvalidLines() {
+        // given
         val context = ApplicationProvider.getApplicationContext<Context>()
-        context.resources.assets.open("workout-test-data.txt")
-        // val workouts = prepopulateWorkoutHistory.createFromAssets("workout-test-data.txt")
-        // workouts.size.shouldBe(10)
-    }
+        prepopulateWorkoutHistory = PrepopulateWorkoutHistoryImpl(mapper)
 
-    @Test
-    fun mapToEntity() {
-        val entity = "ds"
+        // when
+        val data = prepopulateWorkoutHistory.createFromStream(context.resources.assets.open("workout-test-data.txt"))
+
+        // then
+        data.size.shouldBe(6)
+        data.forEach {
+            it.exerciseName.shouldNotBeEmpty()
+            it.reps.shouldBeGreaterThan(0)
+            it.sets.shouldBeGreaterThan(0)
+            it.weight.shouldBeGreaterThan(0)
+        }
     }
 }
